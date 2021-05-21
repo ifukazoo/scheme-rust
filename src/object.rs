93671bf485abfa4090876c675dede400466799d0
ifukazoo <ifukazoo@gmail.com>
2021-05-22 08:07:11 +0900

    四則演算の実装

--- src/object.rs
+++ src/object.rs
@@ -1,6 +1,7 @@
 extern crate num;
-use num::rational::Rational64;
+use num::rational::{Ratio, Rational64};
 use std::fmt;
+use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Number {
@@ -9,19 +10,76 @@ pub enum Number {
     /// 整数
     Int(i64),
 }
+impl Number {
+    /// 正規化
+    fn normal(self) -> Self {
+        match self {
+            Self::Int(_) => self,
+            Self::Rat(r) => {
+                if r.is_integer() {
+                    Number::Int(r.to_integer() as i64)
+                } else {
+                    self
+                }
+            }
+        }
+    }
+
+    /// 有理数にする
+    fn to_rat(self) -> Rational64 {
+        match self {
+            Self::Int(n) => Ratio::from_integer(n),
+            Self::Rat(r) => r,
+        }
+    }
+}
 
 impl fmt::Display for Number {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Self::Rat(r) => {
-                if *r.denom() == 1 {
-                    write!(f, "{}", r.numer())
-                } else {
-                    write!(f, "{}/{}", r.numer(), r.denom())
-                }
-            }
+        let n = self.normal();
+        match n {
+            Self::Rat(r) => write!(f, "{}", r),
             Self::Int(i) => write!(f, "{}", i),
         }
+    }
+}
+
+macro_rules! expr {
+    ($e:expr) => {
+        $e
+    };
+}
+macro_rules! calc {
+    ($s:expr, $op:tt, $o:expr) => {{
+        let lhs = $s.to_rat();
+        let rhs = $o.to_rat();
+        let result = expr!(lhs $op rhs);
+        Self::Rat(result).normal()
+    }};
+}
+
+impl Add for Number {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        calc!(self, +, other)
+    }
+}
+impl Sub for Number {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        calc!(self, -, other)
+    }
+}
+impl Mul for Number {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        calc!(self, *, other)
+    }
+}
+impl Div for Number {
+    type Output = Self;
+    fn div(self, other: Self) -> Self {
+        calc!(self, /, other)
     }
 }
 #[derive(Debug, Clone, PartialEq)]
@@ -55,6 +113,101 @@ impl fmt::Display for Object {
             }
             Self::Num(n) => write!(f, "{}", n),
             Self::Nil => write!(f, "()"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_number_add() {
+        let tests = vec![
+            ((Number::Int(1), Number::Int(2)), Number::Int(3)),
+            (
+                (Number::Rat(Ratio::new(1, 3)), Number::Rat(Ratio::new(1, 3))),
+                Number::Rat(Ratio::new(2, 3)),
+            ),
+            (
+                (Number::Rat(Ratio::new(1, 3)), Number::Int(1)),
+                Number::Rat(Ratio::new(4, 3)),
+            ),
+            (
+                (Number::Rat(Ratio::new(2, 5)), Number::Rat(Ratio::new(3, 5))),
+                Number::Int(1),
+            ),
+        ];
+        for ((l, r), expected) in tests.into_iter() {
+            assert_eq!(expected, l + r)
+        }
+    }
+
+    #[test]
+    fn test_number_sub() {
+        let tests = vec![
+            ((Number::Int(1), Number::Int(2)), Number::Int(-1)),
+            (
+                (Number::Rat(Ratio::new(2, 3)), Number::Rat(Ratio::new(1, 3))),
+                Number::Rat(Ratio::new(1, 3)),
+            ),
+            (
+                (Number::Rat(Ratio::new(1, 3)), Number::Int(1)),
+                Number::Rat(Ratio::new(-2, 3)),
+            ),
+            (
+                (
+                    Number::Rat(Ratio::new(10, 5)),
+                    Number::Rat(Ratio::new(5, 5)),
+                ),
+                Number::Int(1),
+            ),
+        ];
+        for ((l, r), expected) in tests.into_iter() {
+            assert_eq!(expected, l - r)
+        }
+    }
+
+    #[test]
+    fn test_number_mul() {
+        let tests = vec![
+            ((Number::Int(1), Number::Int(1)), Number::Int(1)),
+            ((Number::Int(2), Number::Int(3)), Number::Int(6)),
+            ((Number::Int(1), Number::Int(0)), Number::Int(0)),
+            (
+                (Number::Rat(Ratio::new(1, 3)), Number::Rat(Ratio::new(2, 3))),
+                Number::Rat(Ratio::new(2, 9)),
+            ),
+            (
+                (Number::Rat(Ratio::new(1, 3)), Number::Int(0)),
+                Number::Int(0),
+            ),
+            (
+                (Number::Rat(Ratio::new(2, 3)), Number::Rat(Ratio::new(9, 3))),
+                Number::Int(2),
+            ),
+        ];
+        for ((l, r), expected) in tests.into_iter() {
+            assert_eq!(expected, l * r)
+        }
+    }
+
+    #[test]
+    fn test_number_div() {
+        let tests = vec![
+            ((Number::Int(1), Number::Int(1)), Number::Int(1)),
+            ((Number::Int(6), Number::Int(3)), Number::Int(2)),
+            (
+                (Number::Rat(Ratio::new(1, 3)), Number::Rat(Ratio::new(2, 3))),
+                Number::Rat(Ratio::new(1, 2)),
+            ),
+            (
+                (Number::Rat(Ratio::new(4, 3)), Number::Rat(Ratio::new(2, 3))),
+                Number::Int(2),
+            ),
+        ];
+        for ((l, r), expected) in tests.into_iter() {
+            assert_eq!(expected, l / r)
         }
     }
 }
